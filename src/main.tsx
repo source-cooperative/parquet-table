03b6689b6369acc966ab08ec580ba051b5a957fe
Sylvain Lesage <severo@rednegra.net>
2025-09-15 12:01:17 +0200

    fix css

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import "hyperparam/global.css";
 import "hyperparam/hyperparam.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import './index.css'
+import "./index.css";
 import App from "./App.tsx";
 
 const app = document.getElementById("app");
@@ -12,5 +12,5 @@ if (!app) throw new Error("missing app element");
 createRoot(app).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
