bdacfe45176f2f4a2810d03979c2a59fbdcc8292
Sylvain Lesage <severo@rednegra.net>
2025-09-15 14:00:21 +0200

    rework the welcome page + font

--- src/Welcome.tsx
+++ src/Welcome.tsx
@@ -4,48 +4,19 @@ export default function Welcome(): ReactNode {
   return (
     <div id="welcome">
       <div>
-        <h1>hyparquet</h1>
-        <h2>in-browser parquet file reader</h2>
-        <div className="badges">
-          <a href="https://www.npmjs.com/package/hyparquet">
-            <img
-              src="https://img.shields.io/npm/v/hyparquet"
-              alt="npm hyparquet"
-            />
-          </a>
-          <a href="https://github.com/hyparam/hyparquet">
-            <img
-              src="https://img.shields.io/github/stars/hyparam/hyparquet?style=social"
-              alt="star hyparquet"
-            />
-          </a>
-        </div>
+        <h1>Parquet viewer</h1>
         <p>
-          Online demo of{" "}
-          <a href="https://github.com/hyparam/hyparquet">hyparquet</a>: a parser
-          for apache parquet files. Uses{" "}
-          <a href="https://github.com/hyparam/hightable">hightable</a> for high
-          performance windowed table viewing.
+          Drag and drop 🫳 a parquet file (or url) to see your parquet data, or
+          click an example:
         </p>
-        <p>
-          Drag and drop a parquet file (or url) to see your parquet data. 👀
-        </p>
-        <p>Example files:</p>
         <ul className="quick-links">
           <li>
             <a
-              className="aws"
-              href="?url=https://hyperparam-public.s3.amazonaws.com/wiki-en-00000-of-00041.parquet"
+              className="source"
+              href="?url=https://data.source.coop/vida/google-microsoft-osm-open-buildings/geoparquet/by_country/country_iso=BOL/BOL.parquet"
             >
-              s3://wiki-en-00000-of-00041.parquet
-            </a>
-          </li>
-          <li>
-            <a
-              className="azure"
-              href="?url=https://hyperparam.blob.core.windows.net/hyperparam/starcoderdata-js-00000-of-00065.parquet"
-            >
-              azure://starcoderdata-js-00000-of-00065.parquet
+              <h2>Open Buildings</h2>
+              <p>A GeoParquet file of building footprints.</p>
             </a>
           </li>
           <li>
@@ -53,15 +24,17 @@ export default function Welcome(): ReactNode {
               className="huggingface"
               href="?url=https://huggingface.co/datasets/codeparrot/github-code/resolve/main/data/train-00000-of-01126.parquet?download=true"
             >
-              huggingface://github-code-00000-of-01126.parquet
+              <h2>GitHub Code</h2>
+              <p>Code files from GitHub in 32 programming languages.</p>
             </a>
           </li>
           <li>
             <a
-              className="github"
-              href="?url=https://raw.githubusercontent.com/hyparam/hyparquet/master/test/files/rowgroups.parquet"
+              className="aws"
+              href="?url=https://hyperparam-public.s3.amazonaws.com/wiki-en-00000-of-00041.parquet"
             >
-              github://rowgroups.parquet
+              <h2>Wikipedia</h2>
+              <p>All the articles from the English Wikipedia.</p>
             </a>
           </li>
         </ul>
