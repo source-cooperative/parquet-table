83dd530ab5fe9af83a0afaf20dcfee2972172e24
Sylvain Lesage <severo@rednegra.net>
2025-09-17 16:33:01 +0200

    forgot a file

---
+++ src/Loading.tsx
@@ -0,0 +1,18 @@
+import { cn } from "hyperparam";
+import type { ReactNode } from "react";
+
+interface LoadingProps {
+  className?: string;
+}
+
+/**
+ * Loading component.
+ * div style can be overridden by className prop.
+ *
+ * @param {Object} props
+ * @param {string | undefined} props.className - additional class names to apply to the div
+ * @returns {ReactNode}
+ */
+export default function Loading({ className }: LoadingProps): ReactNode {
+  return <div className={cn("loading", className)}></div>;
+}
