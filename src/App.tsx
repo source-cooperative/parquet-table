2bbb8eab1e1f16fec6837a635197cdb033c954c5
Sylvain Lesage <severo@rednegra.net>
2025-09-17 16:09:05 +0200

    upgrade deps

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import { useCallback, useEffect, useState } from "react";
 import Dropzone from "./Dropzone.js";
 import Layout from "./Layout.js";
 import { toGeoAwareDf } from "./helpers.js";
-import type { SortableDataFrame } from "hightable";
+import { sortableDataFrame } from "hightable";
 
 export default function App(): ReactNode {
   const params = new URLSearchParams(location.search);
@@ -31,9 +31,9 @@ export default function App(): ReactNode {
     async function setAsyncBuffer(name: string, from: AsyncBufferFrom) {
       const asyncBuffer = await asyncBufferFrom(from);
       const metadata = await parquetMetadataAsync(asyncBuffer);
-      const df = parquetDataFrame(from, metadata, {
+      const df = sortableDataFrame(parquetDataFrame(from, metadata, {
         utf8: false,
-      }) as SortableDataFrame; // parquetDataFrame always produces a SortableDataFrame, but it's not typed as such
+      }))
       // TODO(SL): remove this once hyparquet/hyparparam support geoparquet / geometry columns
       const geoAwareDf = toGeoAwareDf(df, metadata);
       setPageProps({
